98ef4e47130b2644d1412342a70f39f82cad60c9
avan <leechunyin2004@gmail.com>
2026-04-06 18:06:37 +0000

    updated to disable crsf and added manual port

--- app/src/main/java/comp3800sef/config/SecurityConfig.java
+++ app/src/main/java/comp3800sef/config/SecurityConfig.java
@@ -12,9 +12,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll()
-                .anyRequest().authenticated()
-            );
+                .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
